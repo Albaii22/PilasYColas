3952628c7e28121d63fc78c119549bb73519e236
Albaii22 <usr3584@salesianos-lacuesta.net>
2023-06-10 16:17:58 +0200

    feat : Creacion de la clase ejecutable de Hiperdino

---
+++ src/net/hiperdino/EjecutableHiperdino.java
@@ -0,0 +1,76 @@
+package net.hiperdino;
+
+import java.util.Scanner;
+
+public class EjecutableHiperdino {
+    public static void main(String[] args) {
+
+        Cajero cajero = new Cajero("Maria");
+        Scanner scanner = new Scanner(System.in);
+        boolean cajaAbierta = true;
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println("Escoge una opción:  ");
+            System.out.println("1. Abrir caja");
+            System.out.println("2. Añadir un nuevo cliente a la cola");
+            System.out.println("3. Atender un cliente");
+            System.out.println("4. Ver clientes pendientes");
+            System.out.println("5. Cerrar supermercado");
+
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    if (cajaAbierta) {
+                        System.out.println("La caja ya está abierta");
+                    } else {
+                        cajaAbierta = true;
+                        cajero.abrirCaja();
+                    }
+                    break;
+                case 2:
+                    if (cajaAbierta) {
+                        Cliente nuevoCLiente = new Cliente(NombreAleatorio.obtenerNombreAleatorio());
+                        cajero.agregarCliente(nuevoCLiente);
+                        System.out.println("Nuevo cliente añadido a la cola");
+                        System.out.println(nuevoCLiente);
+                    } else {
+                        System.out.println("Caja cerrada");
+                    }
+                    break;
+                case 3:
+                    if (cajero.hayClientesEnCola()) {
+                        Cliente clienteAtendido = cajero.atenderCliente();
+                        System.out.println("Cliente atendido");
+                        System.out.println(clienteAtendido);
+                    } else {
+                        System.out.println("No hay cliente que atender");
+                    }
+                    break;
+                case 4:
+                    if (cajero.hayClientesEnCola()) {
+                        System.out.println("Clientes en la cola del cajero "+ cajero.idCaja());
+                        for (Cliente cliente : cajero.getFilaClientes()) {
+                            System.out.println(cliente.getNombre());
+                        }
+                    } else {
+                        System.out.println("No hay clientes pendientes en la cola");
+                    }
+                    break;
+                case 5:
+                    if (cajaAbierta) {
+                        cajero.cerrarCaja();
+                        cajaAbierta = false;
+                    } else {
+                        System.out.println("La caja ya está cerrada");
+                    }
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Elige una opción");
+
+            }
+        }
+    }
+
+}
